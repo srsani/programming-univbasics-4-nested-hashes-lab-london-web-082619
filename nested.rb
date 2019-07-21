32184f917645391457b9d83c9a862927661a3430
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 21:07:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    d = programmer_hash[:dennis_ritchie]
+    d = programmer_hash[:dennis_ritchie][:languages]
   return d
 end
 
